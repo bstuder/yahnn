cdbe6cb81f48552432cd238b5bbc5570f51d006f
Bruno Studer <studer.bruno@gmail.com>
2018-11-09 15:31:15 +0100

    Add row-application

--- src/Matrix.hs
+++ src/Matrix.hs
@@ -2,8 +2,24 @@ module Matrix where
 
 import qualified Data.Vector as V
 
+
 data Matrix a = Matrix {
     nrow :: !Int,
     ncol :: !Int,
     vec :: V.Vector a
 }
+
+
+chunksOf :: Int -> V.Vector a -> V.Vector (V.Vector a)
+chunksOf n v
+  | n <= 0 = V.empty
+  | V.null v = V.empty
+chunksOf n xs = V.take n xs `V.cons` chunksOf n (V.drop n xs)
+
+
+toRows :: Matrix a -> V.Vector (V.Vector a)
+toRows (Matrix _ col vec) = chunksOf col vec
+
+
+applyRow :: (V.Vector a -> b) -> Matrix a -> V.Vector b
+applyRow f = fmap f  . toRows
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,7 +3,8 @@ import Test.QuickCheck
 import Control.Exception (evaluate)
 
 import Utils (chunksOf)
-import Matrix
+import qualified Matrix as M
+import qualified Data.Vector as V
 
 testChunk = do
     it "no chunk" $
@@ -13,7 +14,11 @@ testChunk = do
     it "uneven chunks" $
         chunksOf 3 [1..8] `shouldBe` [[1,2,3],[4,5,6],[7,8]]
 
-testMatrix = undefined
+mat = M.Matrix 2 3 (V.fromList [1..6])
+
+testMatrix = do
+    it "sum rows" $
+        M.applyRow sum mat `shouldBe` V.fromList [6, 15]
 
 main :: IO ()
-main = hspec $ testChunk
+main = hspec $ testChunk >> testMatrix
