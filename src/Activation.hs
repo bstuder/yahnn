b09827e2152ddf77718adf1e675d03a8905bd35b
Laurent Navarro (lanavarr) <lanavarr@cisco.com>
2018-11-09 20:06:15 +0100

    Add ReLu and Sign activation functions

---
+++ src/Activation.hs
@@ -0,0 +1,10 @@
+module Activation where
+
+import qualified Matrix as M (applyRow, Matrix(..))
+import qualified Data.Vector as V (Vector(..))
+
+reLu ::  (Real a) => M.Matrix a -> V.Vector a
+reLu mat = fmap (\x -> if x < 0 then 0 else x) (M.applyRow sum mat) 
+
+sign ::  (Real a) => M.Matrix a -> V.Vector a
+sign mat = fmap (\x -> if x < 0 then -1 else 1) (M.applyRow sum mat) 
--- test/Spec.hs
+++ test/Spec.hs
@@ -3,24 +3,31 @@ import Test.QuickCheck
 import Control.Exception (evaluate)
 
 import Utils (chunksOf)
+import Activation (reLu, sign)
 import qualified Matrix as M
 import qualified Data.Vector as V (empty, fromList, Vector(..))
 
-vec = V.fromList [1..8]
+vec = V.fromList [-2, 3, 6, 1, -8, -9, 3, -5]
 
 testChunk = do
-    it "empty vector" $
+    it "Empty vector" $
         chunksOf 5 (V.empty :: V.Vector Int) `shouldBe` V.empty
-    it "even chunks" $
-        chunksOf 2 vec `shouldBe` V.fromList (fmap V.fromList [[1, 2], [3, 4], [5, 6], [7, 8]])
-    it "uneven chunks" $
-        chunksOf 3 vec `shouldBe` V.fromList (fmap V.fromList [[1, 2, 3], [4, 5, 6], [7, 8]])
+    it "Even chunks" $
+        chunksOf 2 vec `shouldBe` V.fromList (fmap V.fromList [[-2, 3], [6, 1], [-8, -9], [3, -5]])
+    it "Uneven chunks" $
+        chunksOf 3 vec `shouldBe` V.fromList (fmap V.fromList [[-2, 3, 6], [1, -8, -9], [3, -5]])
 
 mat = M.Matrix 2 4 vec
 
 testMatrix = do
-    it "sum rows" $
-        M.applyRow sum mat `shouldBe` V.fromList [10, 26]
+    it "Sum rows" $
+        M.applyRow sum mat `shouldBe` V.fromList [8, -19]
+
+testActivation = do
+    it "Activation sign" $
+        sign mat `shouldBe` V.fromList [1, -1]
+    it "Activation ReLu" $
+        reLu mat  `shouldBe` V.fromList [8, 0]
 
 main :: IO ()
-main = hspec $ testChunk >> testMatrix
+main = hspec $ testChunk >> testMatrix >> testActivation
