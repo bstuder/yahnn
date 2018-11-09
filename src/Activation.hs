28f6e1ec165191471ee517ac143c254eddc79137
Laurent Navarro (lanavarr) <lanavarr@cisco.com>
2018-11-09 20:17:21 +0100

    Remove terrible double-space mistake (v2)

--- src/Activation.hs
+++ src/Activation.hs
@@ -6,5 +6,5 @@ import qualified Data.Vector as V (Vector(..))
 reLu ::  (Real a) => M.Matrix a -> V.Vector a
 reLu mat = fmap (\x -> if x < 0 then 0 else x) (M.applyRow sum mat) 
 
-sign ::  (Real a) => M.Matrix a -> V.Vector a
+sign :: (Real a) => M.Matrix a -> V.Vector a
 sign mat = fmap (\x -> if x < 0 then -1 else 1) (M.applyRow sum mat) 
