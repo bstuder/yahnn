3a71173f7be1065f2dc9ee5aeca4ab6c0dc011f6
Bruno Studer <studer.bruno@gmail.com>
2018-11-09 11:33:01 +0100

    less do

--- test/Spec.hs
+++ test/Spec.hs
@@ -5,11 +5,11 @@ import Control.Exception (evaluate)
 import Utils (chunksOf)
 
 testChunk = do
-    it "no chunk" $ do
+    it "no chunk" $
         chunksOf (-6) [1..6] `shouldBe` []
-    it "even chunks" $ do
+    it "even chunks" $
         chunksOf 2 [1..6] `shouldBe` [[1,2],[3,4],[5,6]]
-    it "uneven chunks" $ do
+    it "uneven chunks" $
         chunksOf 3 [1..8] `shouldBe` [[1,2,3],[4,5,6],[7,8]]
 
 main :: IO ()
