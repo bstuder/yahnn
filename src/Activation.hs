3bc8c3a1e0db357f9294beaa637bef9ea69a1f70
Ostaijen <mournblade@free.fr>
2018-11-10 12:07:39 +0100

    Add eta-reduction on various functions

--- src/Activation.hs
+++ src/Activation.hs
@@ -3,7 +3,7 @@ module Activation where
 import qualified Data.Vector as V (Vector(..))
 
 reLu ::  (Real a) => V.Vector a -> V.Vector a
-reLu vector = fmap (\x -> if x < 0 then 0 else x) vector
+reLu = fmap (\x -> if x < 0 then 0 else x)
 
 sign :: (Real a) => V.Vector a -> V.Vector a
-sign vector = fmap (\x -> if x < 0 then -1 else 1) vector
+sign = fmap (\x -> if x < 0 then -1 else 1)
