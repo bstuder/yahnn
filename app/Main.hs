8a3b40b7c65bc760e5974f2c43fd06c6333a8fba
Bruno Studer <studer.bruno@gmail.com>
2018-11-22 19:00:46 +0100

    do-in-do indentation

--- app/Main.hs
+++ app/Main.hs
@@ -13,11 +13,17 @@ main = do
     let datapoint = V.fromList [1, -3, 2, 7, 12, 5, 2, 6, -5, 0]
 
     let network = N.fromList [10, 5, 2, 5, 10] [A.ReLu, A.ReLu, A.ReLu, A.ReLu] generator
+
     let forwardResult = network >>= N.forward datapoint
     print forwardResult
 
-    let gradient = network >>= (\justNetwork -> forwardResult >>= (\justForward -> N.backward justNetwork justForward datapoint L.MSE))
+    let gradient = do justNetwork <- network
+                      justForward <- forwardResult
+                      N.backward justNetwork justForward datapoint L.MSE
+
     print gradient
 
-    let newNetwork = network >>= (\justNetwork -> gradient >>= (\justGradient -> O.apply justNetwork justGradient (O.SGD 1 0.01)))
+    let newNetwork = do justNetwork <- network
+                        justGradient <- gradient
+                        O.apply justNetwork justGradient (O.SGD 1 0.01)
     print newNetwork
