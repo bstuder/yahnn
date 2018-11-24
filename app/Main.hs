48ebd054f53e29ef112d51236c3914eb303c500a
Bruno Studer <studer.bruno@gmail.com>
2018-11-24 17:00:28 +0100

    working example of data parsing

--- app/Main.hs
+++ app/Main.hs
@@ -7,15 +7,20 @@ import qualified Network as N
 import qualified Optimizer as O
 import qualified System.Random as SR (mkStdGen)
 
+import Data.Binary.Get
+import qualified Data.ByteString.Lazy as BSL
+import qualified Data.ByteString as BS
+
+parseImageFile = do
+    magic <- getWord32be
+    numImages <- getWord32be
+    numRows <- getWord32be
+    numColumns <- getWord32be
+    rawData <- getByteString $ fromIntegral (numImages * numRows * numColumns)
+    let vec = BS.unpack rawData
+    return $! (magic, numImages, numRows, numColumns, vec)
+
 main :: IO ()
 main = do
-    let generator = SR.mkStdGen 12345
-    let dataset = [
-            DV.fromList [1, -3, 2, 7, 12, 5, 2, 6, -5, 0],
-            DV.fromList [8, 1, -5, 4, -3, -8, 4, 4, 0, 7],
-            DV.fromList [-1, 7, 3, 0, -5, -3, 7, 6, -1, 4],
-            DV.fromList [0, -2, 4, 9, -2, -4, 3, -5, -7, 4],
-            DV.fromList [-7, -2, -4, 3, 0, 0, -4, 1, 5, 5]
-            ]
-    let network = N.fromList [10, 5, 2, 5, 10] [A.ReLu, A.ReLu, A.ReLu, A.ReLu] generator
-    print $ network >>= N.train (O.SGD 1 0.01) L.MSE dataset dataset
+    input <- BSL.readFile "data/train-images-idx3-ubyte"
+    print $ runGet parseImageFile input
