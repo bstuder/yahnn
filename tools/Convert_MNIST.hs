6a6906a7dae2142c47d27dcd219e9e432c756f2e
Ostaijen <mournblade@free.fr>
2018-11-25 11:46:33 +0100

    Add changes following PR comments

--- tools/Convert_MNIST.hs
+++ tools/Convert_MNIST.hs
@@ -2,8 +2,9 @@ module Main where
 
 import qualified Data.Binary.Get as DBG
 import qualified Data.ByteString.Lazy as DBL
-import qualified Data.Vector as DV (fromList)
+import qualified Data.Vector as DV (fromList, singleton, Vector(..))
 import qualified Dataset as D (Dataset(..), toByteString)
+import qualified GHC.Word as GW (Word8(..))
 
 chunksOf :: Int -> [a] -> [[a]]
 chunksOf length list
@@ -11,19 +12,21 @@ chunksOf length list
     | null list = []
     | otherwise = take length list : chunksOf length (drop length list)
 
+parseData :: DBG.Get [DV.Vector GW.Word8]
 parseData = do
     magicNumber <- DBG.getInt32be
     numberOfItems <- DBG.getInt32be
     rows <- DBG.getInt32be
     columns <- DBG.getInt32be
     rawData <- DBG.getLazyByteString $ fromIntegral (numberOfItems * rows * columns)
-    return $! map (DV.fromList) $ chunksOf (fromIntegral (rows * columns)) $ DBL.unpack rawData
+    return $! fmap (DV.fromList) $ chunksOf (fromIntegral (rows * columns)) $ DBL.unpack rawData
 
+parseLabels :: DBG.Get [DV.Vector GW.Word8]
 parseLabels = do
     magicNumber <- DBG.getInt32be
     numberOfItems <- DBG.getInt32be
     rawData <- DBG.getLazyByteString $ fromIntegral numberOfItems
-    return $! map (DV.fromList) $ chunksOf 1 $ DBL.unpack rawData
+    return $! fmap (DV.singleton) $ DBL.unpack rawData
 
 main :: IO ()
 main = do
