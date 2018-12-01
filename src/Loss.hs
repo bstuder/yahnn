2ea3944515addcd5265e21d98b8d847a4468762e
Ostaijen <mournblade@free.fr>
2018-12-01 23:23:49 +0100

    Fix typing error in MSE

--- src/Loss.hs
+++ src/Loss.hs
@@ -17,4 +17,4 @@ backward :: RealFloat a => Loss -> M.Matrix a -> M.Matrix a -> Either String (M.
 backward MSE (M.ColumnVector outputSize outputVector) (M.ColumnVector targetSize targetVector)
     | outputSize /= targetSize = Left "Mismatching dimensions between output and target"
     | otherwise = M.fromVector 1 outputSize $ (/ fromIntegral outputSize) . (*2) <$> DV.zipWith (-) outputVector targetVector
-derivate MSE _ _ = Left "Wrong matrix types to compute loss backward"
+backward MSE _ _ = Left "Wrong matrix types to compute loss backward"
