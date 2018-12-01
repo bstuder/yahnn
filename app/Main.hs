6402752b29f182c79f5653d9ce50f9ab1370064e
Ostaijen <mournblade@free.fr>
2018-12-01 17:48:25 +0100

    Rewrite the Main to write everything on MNIST / Must be debugged

--- app/Main.hs
+++ app/Main.hs
@@ -2,27 +2,22 @@ module Main where
 
 import qualified Activation as A (Activation(..))
 import qualified Data.ByteString.Lazy as DBL (readFile)
-import qualified Data.Vector as DV (fromList)
-import qualified Dataset as D (Dataset(..), fromByteString, normalize, NormalizationFlag(..))
+import qualified Dataset as D (Dataset(..), fromByteString, normalize, Flag(..))
 import qualified Loss as L (Loss(..))
 import qualified Network as N (Network(..), random, train)
 import qualified Optimizer as O (Optimizer(..))
-import qualified System.Exit as SE (die)
 import qualified System.Random as SR (mkStdGen)
 
 main :: IO ()
 main = do
-    print "Just do it !"
-
-    {-print "Read the training set..."
-    let dataset = do
-            input <- DBL.readFile "data/MNIST/training_set"
-            return $ D.normalize D.Datapoints $ D.fromByteString input
-
-    print "Train the network..."
+    print "Training the network on MNIST..."
+    input <- DBL.readFile "data/MNIST/training_set"
     let generator = SR.mkStdGen 12345
-    let (trainedNetwork, losses) = do
-            network <- N.random [28 * 28, 300, 10] [A.Sigmoid, A.Sigmoid, A.Sigmoid] generator
-            return $ N.train (O.SGD 1 0.01) L.MSE dataset network
 
-    print "Finished..."-}
+    let result = do
+            dataset <- D.normalize D.Datapoints <$> D.fromByteString input
+            network <- N.random [784, 300, 10] [A.Sigmoid, A.Sigmoid, A.Sigmoid] generator
+            N.train (O.SGD 1 0.01) L.MSE dataset network
+
+    either print (print . fst) result
+    print "Training achieved."
--- src/Dataset.hs
+++ src/Dataset.hs
@@ -1,49 +1,62 @@
-{-# LANGUAGE DeriveGeneric, PatternSynonyms #-}
+{-# LANGUAGE DeriveGeneric #-}
 
-module Dataset where
+module Dataset
+(
+    Dataset(..),
+    Flag(..),
+    fromByteString,
+    fromLists,
+    normalize,
+    toByteString
+) where
 
 import qualified Data.ByteString.Lazy as DBL
 import qualified Data.Serialize as DS (decodeLazy, encodeLazy, Serialize)
 import qualified GHC.Generics as GG (Generic(..))
-import qualified Matrix as M (pattern ColumnVector, fromVector, maximum, minimum, Matrix)
+import qualified Matrix as M (fromVector, maximum, minimum, normalize, Matrix)
+
+
+{----- TYPES -----}
 
 data Dataset a = Dataset {
     datapoints :: [M.Matrix a],
     targets :: [M.Matrix a]
 } deriving (Eq, GG.Generic, Show)
 
+data Flag = Both | Datapoints | Targets deriving (Eq, Show)
+
+
+{----- INSTANCES -----}
+
 instance (DS.Serialize a) => DS.Serialize (Dataset a)
+instance Functor Dataset where
+    fmap function (Dataset datapoints targets) = Dataset ((function <$>) <$> datapoints) ((function <$>) <$> targets)
 
-data NormalizationFlag = Both | Datapoints | Targets deriving (Eq, Show)
 
-fromByteString :: (RealFloat a, DS.Serialize a) => DBL.ByteString -> Either String (Dataset a)
-fromByteString byteString = do
-    (Dataset datapoints targets) <- DS.decodeLazy byteString
-    return $ Dataset (convertList datapoints) (convertList targets)
-    where convertList = fmap (fromInteger <$>)
+{----- HIDDEN METHODS -----}
+
+normalizeFlag :: RealFloat a => Flag -> Flag -> [M.Matrix a] -> [M.Matrix a]
+normalizeFlag inputFlag referenceFlag matrices =
+    if (inputFlag == referenceFlag) || (inputFlag == Both)
+        then M.normalize (Just upperBound) (Just lowerBound) <$> matrices
+        else matrices
+  where
+    upperBound = maximum $ M.maximum <$> matrices
+    lowerBound = minimum $ M.minimum <$> matrices    
+
+
+{----- EXPORTED METHODS -----}
+
+fromByteString :: DBL.ByteString -> Either String (Dataset Double)
+fromByteString = DS.decodeLazy
 
 fromLists :: [M.Matrix a] -> [M.Matrix a] -> Either String (Dataset a)
 fromLists datapoints targets
     | length datapoints /= length targets = Left "Mismatching dimensions between datapoints and targets."
     | otherwise = Right $ Dataset datapoints targets
 
-normalize :: RealFloat a => NormalizationFlag -> Dataset a -> Dataset a
-normalize flag (Dataset datapoints targets) = Dataset newDatapoints newTargets
-  where
-    newDatapoints = if (flag == Both) || (flag == Datapoints)
-        then normalizeMatrices datapoints
-        else datapoints
-    newTargets = if (flag == Both) || (flag == Targets)
-        then normalizeMatrices targets
-        else targets
-    normalizeMatrices matrices = normalizeMatrix upperBound lowerBound <$> matrices
-      where
-        upperBound = maximum $ M.maximum <$> matrices
-        lowerBound = minimum $ M.minimum <$> matrices
-        normalizeMatrix upperBound lowerBound matrix =
-            if upperBound == lowerBound
-                then const 1 <$> matrix
-                else (\x -> (2 * x - upperBound - lowerBound) / (upperBound - lowerBound)) <$> matrix
+normalize :: RealFloat a => Flag -> Dataset a -> Dataset a
+normalize flag (Dataset datapoints targets) = Dataset (normalizeFlag flag Datapoints datapoints) (normalizeFlag flag Targets targets)
 
 toByteString :: (DS.Serialize a) => Dataset a -> DBL.ByteString
 toByteString = DS.encodeLazy
